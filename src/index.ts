20e1446d4a16c07e0b33ae8ed6896a04ca470a13
Vlad Saraev <vlad.saraev@xero.com>
2020-04-27 19:03:44 +1200

    added more comments

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import {getOffsets} from './utils/getoffSets';
 import {triggerBot} from './components/trigger/trigger';
 
 getOffsets().then(() => {
+  // this seems to be perfectly fine for memory and cpu usage
+  // we need setInterval to keep this running 
   setInterval(() => {
     triggerBot();
   }, 1);

4db22bebe38440b6af989ba72cf26e5079e351f4
Vlad Saraev <vlad.saraev@xero.com>
2020-06-29 09:27:44 +1200

    minor change

--- src/components/radar/radar.ts
+++ src/components/radar/radar.ts
@@ -1,16 +1,18 @@
 import { writeMemory } from 'memoryjs';
 
 import { Entity } from '../../enums/entity';
-import { entity } from '../../utils/memory';
+import { entity, localPlayer } from '../../utils/memory';
 import { client } from '../../utils/process';
 
 export const radar = () => {
   let i: number;
   for (i = 0; i < 30; i += 1) {
     const player = entity.getEntityPlayer(i);
+    const myCurrentTeam = localPlayer.getLocalPlayerTeam();
+    const entityTeam = entity.getEntityPlayerTeam(i);
     const isDormant = entity.getEntityIsDormant(i);
-    if (!isDormant) {
-      writeMemory(client.processHandle, player + Entity.Spotted, true, 'int');
+    if (!isDormant && myCurrentTeam !== entityTeam) {
+      writeMemory(client.processHandle, player + Entity.Spotted, true, 'boolean');
     }
   }
 };

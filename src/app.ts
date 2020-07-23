8f00c13aefa89e9454c09b0bbdaba5f9ddf604f1
Vlad Saraev <vlad.saraev@xero.com>
2020-07-23 12:36:39 +1200

    changed

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
 import { app, BrowserWindow } from 'electron';
+
+app.on('ready', () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile('./index.html');
+});
